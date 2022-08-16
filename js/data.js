0a5efe5360f1b8a3ed58bcb1141bdded60be6a9b
adrieldussex <adrieldussex@gmail.com>
2022-08-16 16:32:14 -0300

    data js

---
+++ js/data.js
@@ -0,0 +1,32 @@
+let peliculas = [
+    padrino = {
+        imagen: 'https://http2.mlstatic.com/D_NQ_NP_636403-MLA41920779831_052020-O.jpg',
+        nombre: 'El padrino',
+        genero: 'Pelicula clasica',
+        descripcion: 'Descripcion 1',
+    },
+    descanso = {
+        imagen: 'https://www.themoviedb.org/t/p/w500/jGFqZ4bCAYaNZKHQKBZcTyrH1EV.jpg',
+        nombre: 'El descanso',
+        genero: 'Drama',
+        descripcion: 'Descripcion 2',
+    }, 
+    si = {
+        imagen: 'https://mx.web.img3.acsta.net/pictures/21/05/18/20/52/1008145.jpg',
+        nombre: 'Si, senor',
+        genero: 'Comedia',
+        descripcion: 'Descripcion 3',
+    },
+    scream = {
+        imagen: 'https://ih1.redbubble.net/image.1118061337.9507/fposter,small,wall_texture,product,750x1000.jpg',
+        nombre: 'Scream',
+        genero: 'Terror',
+        descripcion: 'Descripcion 4',
+    },
+    mentiroso = {
+        imagen: 'https://www.themoviedb.org/t/p/original/d4HHI6uc8CNvQaLd8cVdk1tXTwE.jpg',
+        nombre: 'Mentiroso, mentiroso',
+        genero: 'Comedia',
+        descripcion: 'Descripcion 5',
+    },
+]
