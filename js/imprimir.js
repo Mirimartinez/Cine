ad04d7fb5e71283176268e3796654ec8c1e08499
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-16 16:19:47 -0300

    agrega funcion imprimir por objeto

---
+++ js/imprimir.js
@@ -0,0 +1,27 @@
+
+console.log(peliculas);
+
+function plantilla(objeto) {
+  return `
+  <div class="pelicula">
+        <div class="pelicula-titulo">
+        <img src=${objeto.imagen} alt="foto-pelicula">
+        <h4>${objeto.nombre}</h4>
+        </div>
+        <div class="pelicula-descripcion">
+        <p>${objeto.genero}</p>
+        <p>${objeto.descripcion}</p>
+        </div>
+        </div>`;
+}
+
+function imprimirEnHtml(objeto){
+    document.querySelector(".container-peliculas").innerHTML += plantilla(objeto)
+}
+
+imprimirEnHtml(descanso)
+imprimirEnHtml(padrino)
+imprimirEnHtml(scream)
+imprimirEnHtml(mentiroso)
+
+
