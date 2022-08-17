15a2d78c91e712ffeb1a43c59c819c83580b26d3
adrieldussex <adrieldussex@gmail.com>
2022-08-17 15:39:06 -0300

    mas peliculas

--- js/data.js
+++ js/data.js
@@ -28,5 +28,41 @@ let peliculas = [
         nombre: 'Mentiroso, mentiroso',
         genero: 'Comedia',
         descripcion: 'Descripcion 5',
+    },
+    joker = {
+        imagen: 'https://dam.smashmexico.com.mx/wp-content/uploads/2019/12/Poster4.jpg',
+        nombre: 'El Joker',
+        genero: 'Drama',
+        descripcion: 'Descripcion 6',
+    },
+    it = {
+        imagen: 'https://img.aullidos.com/imagenes/caratulas/it-2-poster-4.jpg',
+        nombre: 'IT 2',
+        genero: 'Terror',
+        descripcion: 'Descripcion 7',
+    },
+    avatar = {
+        imagen: 'https://i0.wp.com/hipertextual.com/wp-content/uploads/2016/10/avatar-poster-01-600x886.jpg?resize=600%2C886&quality=50&strip=all&ssl=1',
+        nombre: 'Avatar',
+        genero: 'Ciencia Ficcion',
+        descripcion: 'Descripcion 8',
+    },
+    batman = {
+        imagen: 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/JVOMOXEH5FACLF6PAORUHD22PY.jfif',
+        nombre: 'Batman',
+        genero: 'Accion',
+        descripcion: 'Descripcion 9',
+    },
+    shrek = {
+        imagen: 'https://enfilme.com/img/content/shrek_Enfilme_535z8.jpeg',
+        nombre: 'Shrek',
+        genero: 'Animacion',
+        descripcion: 'Descripcion 10',
+    },
+    increibles = {
+        imagen: 'https://depor.com/resizer/_ibOEcgLBpoJ7QHACA-5E8-CczM=/620x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/OFFQIVE6KZHX5JVFIDZ2C2HDFU.jpg',
+        nombre: 'Los increibles 2',
+        genero: 'Animacion',
+        descripcion: 'Descripcion 11',
     }
 ]
